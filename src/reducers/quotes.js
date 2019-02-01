18ded83b07222a05a60cc0b761d71d4fee5e96ea
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-01 19:43:45 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -7,7 +7,7 @@ export default (state = [], action) => {
       idx = state.indexOf(action.quoteId);
       return [state.slice(0, idx), state.slice(idx + 1)]
     case 'UPVOTE_QUOTE':
-      return [...state, state.vote + 1]
+      return [...state, state.votes + 1]
 
     default:
       return state;
