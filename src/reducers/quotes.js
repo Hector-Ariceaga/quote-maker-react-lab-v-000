4eb6f7b52653e7a03ad79003fb6545f9a757f77a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-01 20:25:41 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -6,7 +6,7 @@ export default (state = [], action) => {
     case 'ADD_QUOTE':
       return state.concat(action.quote);
     case 'REMOVE_QUOTE':
-      return state.filter(quote => quote.id === action.id)
+      return state.filter(quote => quote.id === action.quoteId)
     case 'UPVOTE_QUOTE':
 
     case 'DOWNVOTE_QUOTE':
