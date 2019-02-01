9ad8d0ca0dfa394ee242a860dd16324482267661
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-02-01 19:38:39 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -3,6 +3,9 @@ export default (state = [], action) => {
   switch(action.type) {
     case 'ADD_QUOTE':
       return [...state, action.quote];
+    case 'REMOVE_QUOTE':
+      idx = state.indexOf(action.id);
+      return [state.slice(0, idx), state.slice(idx + 1)]
 
     default:
       return state;
